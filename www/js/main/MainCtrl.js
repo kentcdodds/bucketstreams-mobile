064edd61bb5acd92f56e499e05bbf13b0e8eb5b9
Kent C. Dodds <kent@doddsfamily.us>
2014-03-28 05:44:23 -0600

    In broken state in the browser. Working out why cookies are not getting set...

--- www/js/main/MainCtrl.js
+++ www/js/main/MainCtrl.js
@@ -1,5 +1,34 @@
-angular.module('bsm.app').controller('MainCtrl', function($scope, currentUser) {
+angular.module('bsm.app').controller('MainCtrl', function($scope, currentUser, isAuthenticated, User, $state, $ionicPopup, CurrentUserInfoService) {
   $scope.greeting = 'Hello World!';
   console.log(currentUser);
   $scope.currentUser = currentUser;
+  $scope.isAuthenticated = isAuthenticated;
+
+  $scope.$on(CurrentUserInfoService.events.user, function(event, user) {
+    $scope.currentUser = user;
+    $scope.isAuthenticated = !!user;
+  });
+
+  $scope.login = function(input, password) {
+    loginOrRegister('login', input, password);
+  };
+
+  $scope.signUp = function(input, password) {
+    loginOrRegister('register', input, password);
+  };
+
+  function loginOrRegister(action, input, password) {
+    $scope.going = true;
+    User[action](input, password).success(function(data) {
+      CurrentUserInfoService.refreshUser();
+      CurrentUserInfoService.refreshAuthenticated();
+    }).error(function(err) {
+      $scope.going = false;
+      $ionicPopup.alert({
+        content: err.message,
+        title: 'Whoops!',
+        okType: 'button-energized'
+      });
+    });
+  }
 });
